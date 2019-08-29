8dbdb8eb4c7bc3b4f5dc52b15a3874fa41715dbe
Peter Viss <peter.c.viss@biola.edu>
2019-08-29 12:54:20 -0400

    learning components

--- first-angular-app/src/app/app.module.ts
+++ first-angular-app/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
